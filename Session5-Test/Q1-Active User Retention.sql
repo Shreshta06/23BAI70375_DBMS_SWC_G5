3797fd2a7626f11f8f09100ed5dcc252875bb816
Shreshta06 <23bai70375@gmail.com>
2026-06-10 10:31:48 +0530

    Update and rename Session5-Test/Q1-Active User Retention.sql to session5.sql

--- Session5-Test/Q1-Active User Retention.sql
+++
@@ -1,4 +0,0 @@
-select 7 as month , count(distinct(user_id)) as monthly_active_users
-from user_actions u where event_date>='2022-07-01' and event_date<'2022-08-01' and 
-exists(select 1 from user_actions p 
-where p.user_id=u.user_id and p.event_date>='2022-06-01' and p.event_date<'2022-07-01' )
